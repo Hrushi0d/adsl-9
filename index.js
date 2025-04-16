3f874bb7812157119d0ef68e5e6829d1e8f7734f
hrushi0d <hrushi0d@gmail.com>
2025-04-17 00:19:11 +0530

    CRUD routes

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import { Client } from 'cassandra-driver';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 
 // Initialize Express
 const app = express();
@@ -9,6 +10,7 @@ const port = 3000;
 
 // Body Parser middleware
 app.use(bodyParser.json());
+app.use(cors());
 
 // Cassandra setup
 const cassandraClient = new Client({
@@ -53,6 +55,47 @@ app.post('/cassandra', (req, res) => {
     });
 });
 
+// READ all students
+app.get('/cassandra', (req, res) => {
+  const query = 'SELECT * FROM students_table';
+  cassandraClient.execute(query)
+    .then(result => res.json(result.rows))
+    .catch(err => res.status(500).send('Error fetching data: ' + err));
+});
+
+// READ one student by PRN
+app.get('/cassandra/:prn', (req, res) => {
+  const { prn } = req.params;
+  const query = 'SELECT * FROM students_table WHERE prn = ?';
+  cassandraClient.execute(query, [prn], { prepare: true })
+    .then(result => {
+      if (result.rowLength === 0) return res.status(404).send('Student not found');
+      res.json(result.rows[0]);
+    })
+    .catch(err => res.status(500).send('Error fetching student: ' + err));
+});
+
+// UPDATE a student by PRN
+app.put('/cassandra/:prn', (req, res) => {
+  const { prn } = req.params;
+  const { name, department } = req.body;
+
+  const query = 'UPDATE students_table SET name = ?, department = ? WHERE prn = ?';
+  cassandraClient.execute(query, [name, department, prn], { prepare: true })
+    .then(() => res.send('Student updated successfully'))
+    .catch(err => res.status(500).send('Error updating student: ' + err));
+});
+
+// DELETE a student by PRN
+app.delete('/cassandra/:prn', (req, res) => {
+  const { prn } = req.params;
+
+  const query = 'DELETE FROM students_table WHERE prn = ?';
+  cassandraClient.execute(query, [prn], { prepare: true })
+    .then(() => res.send('Student deleted successfully'))
+    .catch(err => res.status(500).send('Error deleting student: ' + err));
+});
+
 // POST to MongoDB
 app.post('/mongo', (req, res) => {
   const { name, prn, department } = req.body;
@@ -70,6 +113,49 @@ app.post('/mongo', (req, res) => {
     .catch(err => {
       res.status(500).send('Error inserting into MongoDB: ' + err);
     });
+});
+
+// READ all students
+app.get('/mongo', (req, res) => {
+  StudentMongo.find()
+    .then(students => res.json(students))
+    .catch(err => res.status(500).send('Error fetching students: ' + err));
+});
+
+// READ one student by PRN
+app.get('/mongo/:prn', (req, res) => {
+  const { prn } = req.params;
+  StudentMongo.findOne({ prn })
+    .then(student => {
+      if (!student) return res.status(404).send('Student not found');
+      res.json(student);
+    })
+    .catch(err => res.status(500).send('Error fetching student: ' + err));
+});
+
+// UPDATE a student by PRN
+app.put('/mongo/:prn', (req, res) => {
+  const { prn } = req.params;
+  const { name, department } = req.body;
+
+  StudentMongo.findOneAndUpdate({ prn }, { name, department }, { new: true })
+    .then(updated => {
+      if (!updated) return res.status(404).send('Student not found');
+      res.send('Student updated successfully');
+    })
+    .catch(err => res.status(500).send('Error updating student: ' + err));
+});
+
+// DELETE a student by PRN
+app.delete('/mongo/:prn', (req, res) => {
+  const { prn } = req.params;
+
+  StudentMongo.findOneAndDelete({ prn })
+    .then(deleted => {
+      if (!deleted) return res.status(404).send('Student not found');
+      res.send('Student deleted successfully');
+    })
+    .catch(err => res.status(500).send('Error deleting student: ' + err));
 });
 
 // Start the server
